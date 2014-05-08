9374d118e7cb9f955536cda6d5ec07fa9ea84603
Tyler <ndvr@Tylers-MacBook-Pro.local>
2014-05-08 15:19:02 -0700

    got rid of Jade, map displays prison

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page.  */
 router.get('/', function(req, res) {
-  res.render('index', {title:'Where is Charles Manson?'});
+  //res.render('index', {title:'Where is Charles Manson?'});
+  res.render('index.html')
 });
 
 
