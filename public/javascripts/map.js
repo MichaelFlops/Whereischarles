6fd9701240415ade09f621436a149c98403b7d8d
endeav0ur <tylsmith@gmail.com>
2014-05-13 14:08:05 -0700

    finished?

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,5 +1,25 @@
 
-window.onload = function(){
-	var map = L.mapbox.map('map', 'ndvr.i689n120')
-    	.setView([40, -74.50], 9);
+function sleep(millis, callback) {
+	setTimeout(callback, millis);
 }
+
+function zoomin(zoomlevel){
+	map.setView([36.05857083768647, -119.54936027526854], zoomlevel)
+
+}
+
+function mapinit(){
+
+    document.getElementById('charles').style.visibility = 'hidden';
+    document.getElementById('title-text').style.visibility = 'hidden';
+    document.getElementById('subtitle').style.visibility = 'hidden';
+
+    document.getElementById('map').style.visibility = 'visible';
+
+	sleep(2500, "zoomin(14)")
+
+}
+
+var map = L.mapbox.map('map', 'ndvr.i689n120')
+.setView([0, 0], 2);
+
